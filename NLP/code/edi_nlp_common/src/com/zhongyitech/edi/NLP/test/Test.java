22610612ba5dca882deb980378f52418b20be8b2
wuyang12 <wuy@zhongyitech.com>
2015-11-20 14:06:08 +0800

    0

--- NLP/code/edi_nlp_common/src/com/zhongyitech/edi/NLP/test/Test.java
+++ NLP/code/edi_nlp_common/src/com/zhongyitech/edi/NLP/test/Test.java
@@ -1,14 +1,14 @@
-package com.zhongyitech.edi.NLP.test;
+package omsaTest.test;
 import java.util.List;
 
-import com.zhongyitech.edi.NLP.model.Opinion;
-import com.zhongyitech.edi.NLP.util.OpMiningUtil;
+import omsaTest.model.Opinion;
+import omsaTest.util.OpMiningUtil;
 
 public class Test {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		List<Opinion> oplist = OpMiningUtil.doSa("雨燕同飞 回复 452讷河市杨爽 ：信号差和电池不耐用是三星通病，用了几个三星手机都是这问题，不过三星的屏幕色彩不错", "sss");
+		List<Opinion> oplist = OpMiningUtil.doSa("屏好，不知道为什么，4G内存占用比例还是很大的，正常都要60%-70%，那个S助手很麻烦，删又删不掉，长期占用内存，三星自带软件太多了，其他运行速度确实很快", "iPhone");
 		for (int i = 0; i < oplist.size(); i++) {
 			System.out.println("op" + i + ":" + "\r\n\t产品：" + oplist.get(i).getProduct().getContent() + "\r\n\t评论对象："
 					+ oplist.get(i).getAspect().getContent() + "\r\n\t评论对象属性："
