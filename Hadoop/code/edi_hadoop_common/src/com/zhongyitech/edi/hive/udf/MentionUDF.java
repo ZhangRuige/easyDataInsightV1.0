7b65f436ee995eaa03149e9299c5e2833e253a35
shizh <shizh@zhongyitech.com>
2015-12-09 18:14:01 +0800

    提及UDF修改返回方式

--- Hadoop/code/edi_hadoop_common/src/com/zhongyitech/edi/hive/udf/MentionUDF.java
+++ Hadoop/code/edi_hadoop_common/src/com/zhongyitech/edi/hive/udf/MentionUDF.java
@@ -1,6 +1,7 @@
 package com.zhongyitech.edi.hive.udf;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.hadoop.hive.ql.exec.UDF;
 
@@ -9,32 +10,22 @@ import com.zhongyitech.edi.NLP.util.RivalProductUtil;
 //用于提及提取的hive transform 自定义类
 public class MentionUDF extends UDF {
 		
-	public ArrayList<ArrayList<String>> evaluate(String comm,String brand, String model) {
-		ArrayList<ArrayList<String>> ps = new ArrayList<ArrayList<String>>();
+	public List<List<String>> evaluate(String cid,String comm/*,String brand, String model*/) {
+		List<List<String>> model_mentions = new ArrayList<List<String>>();
 		try {
 			//提取comm文本的观点
-			String model_mentions = RivalProductUtil.findRivalProduct(comm);
+			model_mentions = RivalProductUtil.findRivalProduct(comm);
 			
-			if(model_mentions!=null && model_mentions.length()>0){
-				String []mentions = model_mentions.split(";");
-				for (String mention : mentions) {
-					ArrayList<String> m = new ArrayList<String>();
-					String []fields = mention.split(",");
-					if((fields[0].equals(brand) || "null".equals(fields[0].toLowerCase())) 
-							&& fields[1].equals(model)){	//排除自己,提到和自己相同的型号，但没提品牌时认为是指自己
-						continue;
-					}
-					
-					m.add(fields[0]);
-					m.add(fields[1]);
-					ps.add(m);
+			if( model_mentions.size() > 0 ){
+				for (List<String> mention : model_mentions) {
+					mention.add(0, cid);
 				}
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
-		return ps;
+		return model_mentions;
 	}
 
 }
