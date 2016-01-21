ad02c52d6e6770fbf9b654ab99aa887fef00577f
shizh <shizh@zhongyitech.com>
2016-01-21 15:38:06 +0800

    修改edi_edploy.sh

--- Hadoop/code/script/deploy/edi_deploy.sh
+++ Hadoop/code/script/deploy/edi_deploy.sh
@@ -1,35 +1,40 @@
-#!/bin/sh
-#######
-# make(compile|jar) and deploy edi FROM jenkins
-#######
+#!/bin/bash
+##
+# 本脚本用来对已经构建编译成功的文件的部署，在完后edi所有编译打包后被执行。
+# 本脚步只负责将指定的文件或目录copy到指定目标位置，不负责对被复制文件的检验。
+##
 
 ## 0.init
-DEPLORY_BASE_DIR=/opt/running/edi/
-JENKINS_WD=/var/lib/jenkins/workspace/edi/default
+JENKINS_EDI_WD=/var/lib/jenkins/workspace/edi/default		#edi
+JENKINS_SPD_WD=/var/lib/jenkins/workspace/edi_crawl/default	#spider
+RUNNING_EDI_DIR=hadoop@hadoopnamenode:/opt/running/edi	#edi 部署路径
+RUNNING_SPD=hadoop@crawljd					#edi 部署路径
 
-## 1.make jar com.zhongyitech.edi.NLP.omsa-v1.25.jar
-sudo ant make-jar
-ecode=$?
-if [ $ecode -ne 0 ];then
-	echo "make-jar NLP failed.exit $ecode"
-	exit $ecode
-fi
+############################# 1.deploy edi(Hadoop,NLP) #############################
+#1) edi script overwrite
+scp -r $JENKINS_EDI_WD/Hadoop/code/script/edi/* $RUNNING_EDI_DIR/
+scp -r $JENKINS_EDI_WD/NLP/code/edi_nlp_common/corpus $RUNNING_EDI_DIR/op/
+scp -r $JENKINS_EDI_WD/NLP/code/edi_nlp_common/dicts $RUNNING_EDI_DIR/op/
+scp -r $JENKINS_EDI_WD/NLP/code/edi_nlp_common/library $RUNNING_EDI_DIR/op/
+scp -r $JENKINS_EDI_WD/NLP/code/edi_nlp_common/model $RUNNING_EDI_DIR/op/
 
-## 2.make jar com.zhongyitech.edi.hive.udf.donlp-1.1.jar
-cd $JENKINS_WD
-sudo ant make-jar
-ecode=$?
-if [ $ecode -ne 0 ];then
-	echo "make-jar HADOOP failed.exit $ecode"
-	exit $ecode
-fi
+#2) edi jars overwrite
+scp $JENKINS_EDI_WD/Hadoop/code/edi_hadoop_common/dist/*.jar $RUNNING_EDI_DIR/op/lib/
+scp $JENKINS_EDI_WD/Hadoop/code/edi_nlp_common/dist/*.jar $RUNNING_EDI_DIR/op/lib/
+scp $JENKINS_EDI_WD/Hadoop/code/edi_hadoop_common/lib/*.jar $RUNNING_EDI_DIR/op/lib/
+scp $JENKINS_EDI_WD/Hadoop/code/edi_nlp_common/lib/*.jar $RUNNING_EDI_DIR/op/lib/
+
+#3) Mysql database backup script overwrite	
+scp -r $JENKINS_EDI_WD/Hadoop/code/script/db_bk hadoop@hadoopmysql:./1
 
 
+############################# 2.deploy spider #############################
+#1) nutch-crawl 
+tar -cvf $JENKINS_SPD_WD/nutch-crawl/runtime $JENKINS_SPD_WD/nutch-crawl/dist/runtime.tar
+scp -r $JENKINS_SPD_WD/nutch-crawl/dist/runtime.tar $RUNNING_SPD_DIR:./soft/
+ssh $RUNNING_SPD_DIR tar -zxf ./soft/runtime.tar ./soft/
 
-## 3.deploy
-sudo scp $JENKINS_WD/Hadoop/code/edi_hadoop_common/dist/*.jar $DEPLORY_BASE_DIR/op/lib/
-sudo scp $JENKINS_WD/Hadoop/code/edi_nlp_common/dist/*.jar $DEPLORY_BASE_DIR/op/lib/
-
-sudo scp $JENKINS_WD/Hadoop/code/edi_hadoop_common/lib/*.jar $DEPLORY_BASE_DIR/op/lib/
-sudo scp $JENKINS_WD/Hadoop/code/edi_nlp_common/lib/*.jar $DEPLORY_BASE_DIR/op/lib/
-
+#2) ParseCrawlData
+tar -cvf $JENKINS_SPD_WD/ParseCrawlData/ParseCrawl $JENKINS_SPD_WD/ParseCrawlData/ParseCrawl.tar
+scp -r $JENKINS_SPD_WD/ParseCrawlData/ParseCrawl.tar $RUNNING_SPD_DIR:./soft/
+ssh $RUNNING_SPD_DIR tar -zxf ./soft/ParseCrawl.tar ./soft/
